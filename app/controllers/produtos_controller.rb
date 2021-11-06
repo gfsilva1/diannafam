d6945ef89a2db1ab577171f85f16e7124f2ade56
Guilherme Silva <gfsilva@hotmail.com>
2021-11-06 16:57:07 -0300

    rev2

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -20,10 +20,19 @@ class ProdutosController < ApplicationController
     redirect_to produto_path(@produto)
   end
 
+  def destroy
+
+    @produto = Produto.find(params[:id])
+    @produto.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to produtos_path
+  end
+
   private
 
   def produto_params
-    params.require(:produto).permit(:nome, :cor, :tamanho, :valor)
+    params.require(:produto).permit(:nome, :cor, :tamanho, :valor, :photo)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "produtos/new", to: "produtos#new"
   post "produtos", to: "produtos#create"
   get  "produtos/:id", to: "produtos#show", as: :produto
+  delete "produtos/:id", to: "produtos#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
